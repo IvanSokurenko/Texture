23b87a6517f710db090d9de26d75df37b9bd1417
Ivan Sokurenko <sokurenko.vanya@gmail.com>
2018-04-16 16:22:23 +0300

    fixed params;

--- AsyncDisplayKit/Private/ASDefaultPlaybackButton.m
+++ AsyncDisplayKit/Private/ASDefaultPlaybackButton.m
@@ -39,15 +39,15 @@
   }
 }
 
-- (nullable id<NSObject>)drawParametersForAsyncLayer:(_ASDisplayLayer *)layer
+- (nullable NSDictionary *)drawParametersForAsyncLayer:(_ASDisplayLayer *)layer
 {
   return @{
-    @"buttonType" : [NSNumber numberWithInt:_buttonType],
+    @"buttonType" : @(self.buttonType),
     @"color" : self.tintColor
   };
 }
 
-+ (void)drawRect:(CGRect)bounds withParameters:(id<NSObject>)parameters isCancelled:(asdisplaynode_iscancelled_block_t)isCancelledBlock isRasterizing:(BOOL)isRasterizing
++ (void)drawRect:(CGRect)bounds withParameters:(NSDictionary *)parameters isCancelled:(asdisplaynode_iscancelled_block_t)isCancelledBlock isRasterizing:(BOOL)isRasterizing
 {
   ASDefaultPlaybackButtonType buttonType = [parameters[@"buttonType"] intValue];
   UIColor *color = parameters[@"color"];
